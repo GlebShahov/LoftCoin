74d3c187489b9e9d980d45ddf0520dd0302c3c0f
GlebShahov <shahovgleb@yandex.ru>
2019-03-17 12:03:43 +0500

    convert png to webp

--- app/src/main/java/com/example/loftcoin/screens/welcome/WelcomeActivity.java
+++ app/src/main/java/com/example/loftcoin/screens/welcome/WelcomeActivity.java
@@ -3,11 +3,13 @@ package com.example.loftcoin.screens.welcome;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.loftcoin.App;
 import com.example.loftcoin.R;
 import com.example.loftcoin.data.prefs.Prefs;
+import com.example.loftcoin.screens.start.StartActivity;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -48,7 +50,14 @@ public class WelcomeActivity extends AppCompatActivity {
         final Prefs prefs = ((App) getApplication()).getPrefs();
         pager.setAdapter(new WelcomePagerAdapter(getSupportFragmentManager()));
         tabDots.setupWithViewPager(pager, true);
+        startBtn.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                prefs.setFirstLaunch(false);
+                StartActivity.startInNewTask(WelcomeActivity.this);
+            }
+        });
 
     }
 }
