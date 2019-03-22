23387e45abed6ad397bc79b80985e66061186b71
GlebShahov <shahovgleb@yandex.ru>
2019-03-22 13:54:06 +0500

    change string convert

--- app/src/main/java/com/example/loftcoin/data/api/Api.java
+++ app/src/main/java/com/example/loftcoin/data/api/Api.java
@@ -10,7 +10,7 @@ import retrofit2.http.Query;
 
 public interface Api {
 
-    String CONVERT = "USD, EUR, RUB";
+    String CONVERT = "USD,EUR,RUB";
     @GET("cryptocurrency/listings/latest")
     @Headers("X-CMC_PRO_API_KEY: 503bfad8-c36b-48c2-9174-97bae3561d17")
     Call<RateResponse> rates(@Query("convert") String convert);
