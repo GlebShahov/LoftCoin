935b8d8607fba81273650479bcc206237ce79ae7
GlebShahov <shahovgleb@yandex.ru>
2019-04-03 20:38:42 +0500

    add livedata

--- app/src/main/java/com/example/loftcoin/data/db/room/AppDatabase.java
+++ app/src/main/java/com/example/loftcoin/data/db/room/AppDatabase.java
@@ -1,15 +1,16 @@
 package com.example.loftcoin.data.db.room;
 
 import com.example.loftcoin.data.db.model.CoinEntity;
+import com.example.loftcoin.data.db.model.Wallet;
 
-import androidx.annotation.NonNull;
+
 import androidx.room.Database;
-import androidx.room.DatabaseConfiguration;
-import androidx.room.InvalidationTracker;
-import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
-@Database(entities = {CoinEntity.class}, version = 2, exportSchema = false)
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {CoinEntity.class, Wallet.class}, version = 3, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CoinDao coinDao();
+    public abstract WalletDao walletDao();
 }
